5ba37758fc075247f39a807c413f0a490a98c455
GreenMan36 <5887406+GreenMan36@users.noreply.github.com>
2021-09-23 12:13:03 +0200

    FA1's

--- Programming/FA1.py
+++ Programming/FA1.py
@@ -1,3 +1,4 @@
 uurloon = float(input("Wat is uw uurloon?\n"))
 uren = float(input("Hoeveel uur heeft u gewerkt?\n"))
-print(f"{uren} uur levert €{round(uren*uurloon*100)/100} op.")
+salaris= round(uren*uurloon*100)/100
+print(f"{uren} uur levert €{salaris} op.")
