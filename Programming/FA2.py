4b9a11655d26340554e81ebe9d213dc9aaf442bc
GreenMan36 <5887406+GreenMan36@users.noreply.github.com>
2021-09-30 17:30:23 +0200

    FA 2

--- Programming/FA2.py
+++ Programming/FA2.py
@@ -10,13 +10,13 @@ def standaardprijs(afstandKM):
         return afstandKM * .8               # return afstand maal 80 cent
     elif afstandKM > 50:                    # afstand is meer dan 50
         return 15 + afstandKM * .6          # return vast bedrag plus afstand maal 60 cent
-    return 0.0                              # afstand is kleiner dan 0 dus return 0 euro, hier had ik eerst een else return maar
-                                            # dat bleek dubbel op te zijn. 0.0 is ingevoerd omdat de output een float moet zijn
+    return 0.0                              # afstand is kleiner dan 0 dus return 0 euro, 0.0 is ingevoerd omdat
+                                            # de output een float moet zijn volgens de opdracht
 
 
 def ritprijs(leeftijd, weekendrit, afstandKM):
 
-    stprijs = standaardprijs(afstandKM)      #verminderd function calls, stprijs = standaardprijs
+    stprijs = standaardprijs(afstandKM)      #scheelt herhaling
 
     if weekendrit:                           #if True
         if leeftijd < 12 or leeftijd >= 65:
@@ -29,6 +29,8 @@ def ritprijs(leeftijd, weekendrit, afstandKM):
         else:
             return stprijs                   #geen weekend en geen korting leeftijd, betaald standaardprijs
                                              #is bij de functie standaardprijs() al een float
+
+# 2 missende cases waren: case(65, True, -51, 0.0), case(65, False, -51, 0.0) en zijn toegevoegd
 
 def development_code():
     print("\nDevelopment_code() staat aan\n")
@@ -126,9 +128,16 @@ def __run_tests():
 if __name__ == '__main__':
     module_runner()
 
+"""
+==========================[ Eigen input ]================================
+Hieronder staat een stuk code dat toegang geeft tot eigen input.
+"""
+
+"""
 print("\n======= User program =======")
 leeftijd = int(input("Wat is uw leeftijd?\n"))
 weekend = bool(input("Reist u in het weekend?\n"))
 afstandKM = float(input("Hoeveel KM reist U?\n"))
 ritprijs= ritprijs(leeftijd, weekend, afstandKM)
 print(f"Uw rit kost €{ritprijs:.2f}")
+"""
